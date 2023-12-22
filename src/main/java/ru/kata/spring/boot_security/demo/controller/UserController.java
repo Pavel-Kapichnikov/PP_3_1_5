5e97af3feddc06032135ad9284ad7d0894240590
Pavel Kapichnikov <paval.kap@gmail.com>
2023-12-22 22:16:52 +0300

    init

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,6 +1,9 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +14,9 @@ import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Controller
@@ -24,7 +29,17 @@ public class UserController {
 
     @GetMapping("/admin")
     public String adminPage(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails ud = (UserDetails) authentication.getPrincipal();
+
+        Set<Role> allRoles = new HashSet<>();
+            allRoles.add(new Role("ROLE_USER"));
+            allRoles.add(new Role("ROLE_ADMIN"));
+
+        model.addAttribute("allRoles", allRoles);
+        model.addAttribute("currentUser", userService.getUserByUsername(ud.getUsername()));
         model.addAttribute("users", userService.getAllUsers());
+        //model.addAttribute("allRoles", Arrays.asList("ROLE_USER", "ROLE_ADMIN"));
         return "admin";
     }
 
@@ -56,21 +71,50 @@ public class UserController {
         return "redirect:/admin";
     }
 
+    /*
     @GetMapping("/edit")
     public String editor(@RequestParam(value = "id") long id, Model model) {
-        model.addAttribute("user", userService.getUserById(id));
-        for (Role role : userService.getUserById(id).getRoles()) {
-            if ("ROLE_USER".equals(role.getName())) {
-                model.addAttribute("userRole", true);
+        model.addAttribute("user1", userService.getUserById(id));
+            for (Role role : userService.getUserById(id).getRoles()) {
+                if ("ROLE_USER".equals(role.getName())) {
+                    model.addAttribute("userRole", true);
+                }
+                if ("ROLE_ADMIN".equals(role.getName())) {
+                    model.addAttribute("adminRole", true);
+                }
             }
-            if ("ROLE_ADMIN".equals(role.getName())) {
-                model.addAttribute("adminRole", true);
-            }
-        }
-        return "editor";
-    }
+        return "admin";
+    }*/
 
     @PostMapping("/edit")
+    public String edit(@ModelAttribute("user") User user,
+                       @RequestParam(value = "id") long id,
+                       @RequestParam("selectedRoles") List<String> selectedRoles) {
+        System.out.println("Received User: " + user);
+        System.out.println("Received ID: " + id);
+        System.out.println("Received User Name: " + user.getName());
+        System.out.println("Received Selected Roles: " + selectedRoles);
+
+        Set<Role> tempRoles = new HashSet<>();
+        for (String role : selectedRoles) {
+            tempRoles.add(new Role(role));
+        }
+
+        user.setRoles(tempRoles);
+        userService.editUser(id, user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/delete")
+    public String deleteUser(@RequestParam(value = "id") long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+}
+
+/*
+
+@PostMapping("/admin/edit")
     public String edit(@ModelAttribute("user") User user,
                        @RequestParam(value = "id") long id,
                        @RequestParam(name = "ROLE_USER", defaultValue = "false") boolean userRole,
@@ -87,9 +131,4 @@ public class UserController {
         return "redirect:/admin";
     }
 
-    @GetMapping("/delete")
-    public String deleteUser(@RequestParam(value = "id") long id) {
-        userService.deleteUser(id);
-        return "redirect:/admin";
-    }
-}
+ */
--- src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
@@ -3,6 +3,7 @@ package ru.kata.spring.boot_security.demo.models;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -63,5 +64,18 @@ public class Role implements GrantedAuthority {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
